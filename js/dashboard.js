dff6afb942263613705880c55df6c7229a27a63f
Lungisa Mkhize <iamlungs7@gmail.com>
2026-07-20 06:27:49 +0200

    Test dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,4 @@
+alert("GODO AI Dashboard JS Loaded");
 // ==========================
 // GODO AI Dashboard Engine
 // ==========================
@@ -17,3 +18,30 @@ const lastUpdate = new Date().toLocaleString();
 console.log("Status :", platformStatus);
 console.log("Environment :", environment);
 console.log("Updated :", lastUpdate);
+
+// ==========================
+// Read Statistics JSON
+// ==========================
+
+fetch("assets/data/signal_statistics.json")
+.then(response => response.json())
+.then(data => {
+
+document.getElementById("totalSignals").innerText =
+"Signals : " + data.total_signals;
+
+document.getElementById("wins").innerText =
+"Wins : " + data.wins;
+
+document.getElementById("losses").innerText =
+"Losses : " + data.losses;
+
+document.getElementById("breakeven").innerText =
+"Breakeven : " + data.breakeven;
+
+})
+.catch(error => {
+
+console.log(error);
+
+});
